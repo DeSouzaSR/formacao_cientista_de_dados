1d200f377498c296a779ade4756fc9fb0156989c
DeSouzaSR <sandro.fisica@gmail.com>
2025-06-09 18:28:02 -0300

    Comecei seção 10 - análise exploratória

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # formacao_cientista_de_dados
 Repositório de Código do Curso "Formação Cientista de Dados - 2025"
+
+- [x] Seção 8
+- [x] Seção 10
+  - [ ] 63 - Preparação 
+  - [ ] 64 - Análise Exploratória
+  - [ ] 65 - Tratamento nos Dados
+  - [ ] 66 - Faça você mesmo
---
+++ secao10/exploratoria.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+import statistics as sts
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'../Formacao_CD/10.Limpeza e tratamento de Dados - Prática em Python/Churn.csv', sep=';')
+
+print('\nVisualização')
+print(dataset.head())
+
+# Renomear
+print('\nRenomear')
+dataset.columns = ['Id', 'Score', 'Estado', 'Genero', 'Idade', 'Patrimonio', 'Saldo', 'Produtos', 'TemCartCredito',
+                   'Ativo', 'Salario', 'Saiu']
+print(dataset.head())
+
+print('\nAnálise Exploratória')
+
+print('Estados:')
+agrupado = dataset.groupby(['Estado']).size()
+print(agrupado)
+agrupado.plot.bar(color = 'gray')
+plt.show()
+
+print('Gênero:')
+agrupado = dataset.groupby(['Genero']).size()
+print(agrupado)
+
+agrupado.plot.bar(color = 'gray')
+plt.show()
+
+print('Describe')
+print(dataset['Score']).describe()

a2ae2c3966522bbe4e839709ae13b2be1254a475
DeSouzaSR <sandro.fisica@gmail.com>
2026-08-17 11:10:56 -0300

    Estruturação do projeto para trabalhar com R

---
+++ secao09/exploracao.R
@@ -0,0 +1,43 @@
+# Exploração dos dados
+
+dados = read.csv("data/raw/Churn.csv", sep = ";", na.strings = "", stringsAsFactors = T)
+head(dados)
+summary(dados)
+
+# Colunas
+# Dar nomes às colunas
+colnames(dados) <- c("Id", "Score", "Estado", "Genero", "Idade", "Patrimônio", 
+                     "Saldo", "Produtos", "TemCartCredito", "Ativo", "Salario", 
+                     "Saiu")
+
+head(dados)
+
+# Explorar dados categóricos
+## Estados
+counts <- table(dados$Estado)
+barplot(counts, main="Estado", xlab = "Estado")
+
+## Gênero
+counts <- table(dados$Genero)
+barplot(counts, main="Gêneros", xlab = "Gêneros")
+
+# Explorar dados numéricos
+## Score
+summary(dados$Score)
+boxplot(dados$Score)
+hist(dados$Score)
+
+## Idade
+summary(dados$Idade)
+boxplot(dados$Idade)
+hist(dados$Idade)
+
+## Saldo
+summary(dados$Saldo)
+boxplot(dados$Saldo)
+hist(dados$Saldo)
+
+# Salário
+summary(dados$Salario)
+boxplot(dados$Salario)
+hist(dados$Salario)
